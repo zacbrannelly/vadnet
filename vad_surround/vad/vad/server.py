4a400e0cb142309e70ca788d6464466d598babe5
Zac Brannelly <101668878@student.swin.edu.au>
2019-02-10 21:30:55 +1100

    Send results back to port 25565

--- vad_surround/vad/vad/server.py
+++ vad_surround/vad/vad/server.py
@@ -1,5 +1,6 @@
 import socket 
 import sys 
+import json
 
 from surround import Surround, Config
 from stages import VadData, ValidateData, VadDetection
@@ -16,10 +17,11 @@ def main():
 
     while True:
         audio_input = []
+        source_addr = None
 
         while len(audio_input) < 48000:
             # Retrieve data from client (9600 samples in bytes = 9600 * 2 bytes (2 bytes per sample))
-            data_bytes, _ = sock.recvfrom(9600 * 2)
+            data_bytes, source_addr = sock.recvfrom(9600 * 2)
 
             # Convert the byte array into an array of float samples (-1 to 1)
             for i in range(0, len(data_bytes), 2):
@@ -33,8 +35,12 @@ def main():
 
         if data.error is None and data.output_data is not None:
             print("Noise: " + str(data.output_data[0] * 100.0) + " Voice: " + str(data.output_data[1] * 100.0))
+
+            # Sending the results back to who made the request
+            sock.sendto(json.dumps({ "noise": data.output_data[0], "voice:": data.output_data[1] }), address=(source_addr[0], 25565))
         else:
             print(data.error)
+            break
         
 
 if __name__ == "__main__":
